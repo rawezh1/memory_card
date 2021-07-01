edb40d8d00b406a1651295e380c4565da36f9630
Rawezh <rawezhmarif@gmail.com>
2021-07-01 17:24:41 +0300

    game logic finished, styling needed

--- src/App.js
+++ src/App.js
@@ -8,17 +8,46 @@ function App() {
   const [bestScore, setBestScore] = useState(0);
   const planetArray = [
     { name: 'Mercury', clicked: false, position: 0 },
-    { name: 'Venus', clicked: false, position: 1  },
-    { name: 'Earth',clicked: false, position: 2 },
-    { name: 'Mars',clicked: false, position: 3 },
-    { name: 'Jupiter',clicked: false, position: 4 },
-    { name: 'Saturn',clicked: false, position: 5 },
+    { name: 'Venus', clicked: false, position: 1 },
+    { name: 'Earth', clicked: false, position: 2 },
+    { name: 'Mars', clicked: false, position: 3 },
+    { name: 'Jupiter', clicked: false, position: 4 },
+    { name: 'Saturn', clicked: false, position: 5 },
   ];
   const [cards, setCards] = useState(planetArray);
   const clickHandler = (e) => {
-    const index = cards.findIndex(x => x.name === e.target.textContent)
-    console.log(index);
+    const index = cards.findIndex((x) => x.name === e.target.textContent);
+
+    if (!cards[index].clicked) {
+      setScore(score + 1);
+      cards[index].clicked = true;
+      setCards(shuffle(cards));
+    } else {
+      if (score > bestScore) {
+        setBestScore(score);
+      }
+      setScore(0);
+      setCards(shuffle(planetArray));
+    }
+    console.log(cards);
   };
+
+  function shuffle(array) {
+    let thisCard = array.length;
+    let rndCard;
+
+    while (thisCard) {
+      rndCard = ~~(Math.random() * thisCard);
+      thisCard--;
+      [array[thisCard].position, array[rndCard].position] = [
+        array[rndCard].position,
+        array[thisCard].position,
+      ];
+      [array[thisCard], array[rndCard]] = [array[rndCard], array[thisCard]];
+    }
+    return array;
+  }
+
   return (
     <div className='App'>
       <ScoreBar currentScore={score} bestScore={bestScore} />
