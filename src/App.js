8d2a4f0958afb8d890c6197a4108c7ed5dc886b0
Rawezh <rawezhmarif@gmail.com>
2021-07-03 14:30:32 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import React, { useState, useEffect } from 'react';
 import ScoreBar from './components/score-bar';
 import CardContainer from './components/card-container';
 import Title from './components/title';
+import mercury from './components/Mercury.png'
+import earth from './components/Earth.png'
+import venus from './components/Venus.png'
+import mars from './components/Mars.png'
+import jupiter from './components/Jupiter.png'
+import saturn from './components/Saturn.png'
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const planetArray = [
-    { name: 'Mercury', clicked: false, position: 0 },
-    { name: 'Venus', clicked: false, position: 1 },
-    { name: 'Earth', clicked: false, position: 2 },
-    { name: 'Mars', clicked: false, position: 3 },
-    { name: 'Jupiter', clicked: false, position: 4 },
-    { name: 'Saturn', clicked: false, position: 5 },
+    { name: 'mercury', clicked: false, position: 0,src:mercury },
+    { name: 'venus', clicked: false, position: 1,src:venus },
+    { name: 'earth', clicked: false, position: 2,src:earth },
+    { name: 'mars', clicked: false, position: 3,src:mars },
+    { name: 'jupiter', clicked: false, position: 4,src: jupiter },
+    { name: 'saturn', clicked: false, position: 5,src: saturn },
   ];
   const [cards, setCards] = useState(planetArray);
   const clickHandler = (e) => {
-    const index = cards.findIndex((x) => x.name === e.target.textContent);
+    const index = cards.findIndex((x) => x.name === e.target.id);
 
     if (!cards[index].clicked) {
       setScore(score + 1);
--- src/components/card-container.js
+++ src/components/card-container.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import './cardContainer.css'
+import mercury from './Mercury.png'
+import earth from './Earth.png'
+import venus from './Venus.png'
+import mars from './Mars.png'
+import jupiter from './Jupiter.png'
+import saturn from './Saturn.png'
 
 const CardContainer = (props) => {
   const createCards = () => {
     return props.cards.map((card,index) => {
       return (
         <div key={index}>
-          <p onClick={props.clickHandler}>{card.name}</p>
-          
+          <img class='card-image' id={card.name} src={card.src} alt='' onClick={props.clickHandler}></img>
         </div>
       );
     });
   };
-  return <div>{createCards()}</div>;
+  return <div id='CardContainer'>{createCards()}</div>;
 };
 
 export default CardContainer;
